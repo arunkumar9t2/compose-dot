3ce7d6c96dcec8177df49d98120ef2f68e91aa1b
Arunkumar <arunkumar9t2@users.noreply.github.com>
2021-05-05 03:37:02 +0800

    Remove dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     id("kotlin-application-conventions")
 }
 
-dependencies {
-    implementation("org.apache.commons:commons-text")
-}
-
 application {
     mainClass.set("dev.arunkumar.compositree.app.AppKt")
 }
